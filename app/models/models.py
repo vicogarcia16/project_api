a439c50ecbdd368d6e1e9f4946e28055e63f7370
vmgn16 <mg_619@hotmail.com>
2025-06-13 19:17:44 -0600

    mod task model

--- app/models/models.py
+++ app/models/models.py
@@ -1,9 +1,12 @@
 from app.db.database import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, ForeignKey
-from datetime import datetime
+from sqlalchemy import String, ForeignKey, Text, DateTime
+from datetime import datetime, timezone
 from typing import List
 
+def utcnow_truncated():
+    return datetime.now(timezone.utc).replace(microsecond=0)
+ 
 class User(Base):
     __tablename__ = "users_practice"
 
@@ -19,9 +22,10 @@ class Task(Base):
     __tablename__ = "tasks_practice"
 
     id: Mapped[str] = mapped_column(String(36), primary_key=True, index=True)
-    title: Mapped[str] = mapped_column(nullable=False)
-    description: Mapped[str] = mapped_column(nullable=False)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
     user_id: Mapped[str] = mapped_column(ForeignKey("users_practice.id"), nullable=False)
-    created_at: Mapped[datetime] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=utcnow_truncated)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=utcnow_truncated, onupdate=utcnow_truncated)
     
     user: Mapped[User] = relationship(back_populates="tasks")
